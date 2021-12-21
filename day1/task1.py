7ec2c0b02fdb4b7437181fcc8d301b8f3c96b0f7
Kristoffer Kirkerud <kirkerudk@gmail.com>
2021-12-22 00:20:44 +0100

    Solution for day 1 of advent of code

---
+++ day1/task1.py
@@ -0,0 +1,19 @@
+﻿with open('./data/task1input.txt') as f:
+    lines = f.readlines()
+    lines = [int(x.replace('\n',"")) for x in lines]
+    prev = None
+    increased = 0
+    decreased = 0
+    na = 0
+    for x in lines:
+      if prev != None:
+        if x > prev:
+          increased+=1
+        elif prev > x:
+          decreased+=1
+        else:
+          na+=1
+      prev = x
+    print("Number of increased:", increased)
+    print("Number of decreased:", decreased)
+    print("Number of N/A:", na)
---
+++ day1/task2.py
@@ -0,0 +1,29 @@
+﻿with open('./data/task1input.txt') as f:
+    lines = f.readlines()
+    lines = [int(x.replace('\n',"")) for x in lines]
+
+    lastSum = None
+    latestNumbers = []
+    increased = 0
+    decreased = 0
+    same = 0
+    hasAchivedThree = False
+    for index, x in enumerate(lines):
+      try:
+        a = lines[index]
+        b = lines[index + 1]
+        c = lines[index + 2]
+        currSum = sum([a,b,c])
+        if lastSum:
+          if currSum > lastSum:
+            increased+=1
+          elif currSum < lastSum:
+            decreased+=1
+          else:
+            same+=1
+        lastSum = currSum
+      except IndexError:
+        break
+    print("Increased:", increased)
+    print("Decreased", decreased)
+    print("Same", same)
