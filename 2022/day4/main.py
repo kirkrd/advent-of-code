64c1def700a744cbb96421891da574e6a82a22ee
Kristoffer Kirkerud <kirkerudk@gmail.com>
2022-12-06 23:30:57 +0100

    solve: day 4 part 1 + part 2

--- 2022/day4/main.py
+++ 2022/day4/main.py
@@ -2,25 +2,48 @@ inputs = open("./input.txt").readlines()
 inputlist = [x.replace('\n', '').split(" ") for x in inputs]
 inputlistfixed = [x[0].split(",") for x in inputlist]
 count = 0
+exampleCount = 0
+countOverlapAtAll = 0
+exampleData = [[range(2, 4), range(6, 8)], [range(2, 3), range(4, 5)],
+               [range(5, 7), range(7, 9)], [range(2, 8), range(3, 7)], [
+    range(6, 6), range(4, 6)],
+    [range(2, 6), range(4, 8)]]
+
+
+def range_fully_covers_other(first: range, second: range):
+    firstList = [*first, first.stop] if first.start != first.stop else [
+        first.start, first.stop]
+    secondList = [*second, second.stop] if second.start != second.stop else [
+        second.start, second.stop]
+    isEveryIn1 = all(x in firstList for x in secondList)
+    isEveryIn2 = all(x in secondList for x in firstList)
+    # print(f"First: {firstList} <-- {first}")
+    # print(f"Second: {secondList} <-- {second}")
+    return isEveryIn1 or isEveryIn2
+
+
+def range_overlap(range1, range2):
+    """Whether range1 and range2 overlap."""
+    x1, x2 = range1.start, range1.stop
+    y1, y2 = range2.start, range2.stop
+    return x1 <= y2 and y1 <= x2
+
+
+for pair in exampleData:
+    if range_fully_covers_other(pair[0], pair[1]):
+        exampleCount += 1
+print(f"Example Reference Count: {exampleCount} (2)")
 for pair in inputlistfixed:
-    rangeElf1 = range(int(pair[0].split("-")[0]), int(pair[0].split("-")[1]))
-    rangeElf2 = range(int(pair[1].split("-")[0]), int(pair[1].split("-")[1]))
-    sameInFirst = True
-    sameInSecond = True
-    for num in rangeElf1:
-        if num in rangeElf2:
-            continue
-        else:
-            sameInFirst = False
-            break
-    for num in rangeElf2:
-        if num in rangeElf1:
-            continue
-        else:
-            sameInSecond = False
-            break
-    if sameInFirst or sameInSecond:
+    firstElfAssignments = pair[0].split("-")
+    secondElfAssigments = pair[1].split("-")
+    elf1Range = range(int(firstElfAssignments[0]), int(firstElfAssignments[1]))
+    elf2Range = range(int(secondElfAssigments[0]), int(secondElfAssigments[1]))
+    if range_fully_covers_other(elf1Range, elf2Range):
         count += 1
+    if range_overlap(elf1Range, elf2Range):
+        countOverlapAtAll += 1
 
 
-print(f"Total elf pairs: {count}. {inputlistfixed.__len__()}")
+print(f"Total Pairs that fully contain each other: {count} (Part 1)")
+print(f"Total Pairs that overlap at all {countOverlapAtAll} (Part 2)")
+print(f"Total Pairs: {inputlistfixed.__len__()}")
